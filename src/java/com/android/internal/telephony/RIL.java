3a14f2cb6765bbd120b3fe065c062cdc7ce48c1e
Andrew Dodd <atd7@cornell.edu>
2013-10-14 18:54:35 -0400

    telephony: Add RILJ wakelock debugging
    Patch originally made by Lee Chu against CM10 for the SMDK4210 RIL
    
    Reworked to provide generic RILJ wakelock debugging for all devices
    
    This causes the log to get spammed if an outstanding unresponded RIL request
    is causing extended wakelocks.  The wakelock timer is reset every time a new
    RIL request gets issued, causing the timeout to reset for "older" requests.
    
    End result is an ongoing RILJ wakelock if the RIL doesn't respond to something.
    
    Rather than purge the offending item, we spam the logs, since a lack of a RIL
    response is usually due to a bug on our end.  (e.g. sending a request we should
    not.)
    
    Original commit at:
    https://github.com/CyanogenMod/android_frameworks_base/commit/44a77728d6576d66ee873d18f72caed8c4cb2732
    
    Change-Id: I2c7c337b9e0b1dba76a13bc40d7319ae0d1ea917

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -90,6 +90,7 @@ class RILRequest {
     //***** Instance Variables
     int mSerial;
     int mRequest;
+    long creationTime;
     Message mResult;
     Parcel mParcel;
     RILRequest mNext;
@@ -122,6 +123,7 @@ class RILRequest {
         }
         rr.mRequest = request;
         rr.mResult = result;
+        rr.creationTime = System.currentTimeMillis();
         rr.mParcel = Parcel.obtain();
 
         if (result != null && result.getTarget() == null) {
@@ -2258,6 +2260,8 @@ public final class RIL extends BaseCommands implements CommandsInterface {
     }
 
     private RILRequest findAndRemoveRequestFromList(int serial) {
+        long removalTime = System.currentTimeMillis();
+        long timeDiff = 0;
         synchronized (mRequestList) {
             for (int i = 0, s = mRequestList.size() ; i < s ; i++) {
                 RILRequest rr = mRequestList.get(i);
@@ -2268,6 +2272,14 @@ public final class RIL extends BaseCommands implements CommandsInterface {
                         mRequestMessagesWaiting--;
                     return rr;
                 }
+                else {
+                    timeDiff = removalTime - rr.creationTime;
+                    if ( timeDiff > mWakeLockTimeout )
+                        {
+                            Rlog.d(RILJ_LOG_TAG,  "No response for [" + rr.mSerial + "] " +
+                                  requestToString(rr.mRequest) + " after " + timeDiff + " milliseconds.");
+                        }
+		}
             }
         }
 
