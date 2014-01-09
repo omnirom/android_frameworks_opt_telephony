8d50a178910db46698c5e2d06cb6d039f9574ff1
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-01-09 20:02:08 +0700

    [3/3] Opt/Telephony: Add MobileNetwork Flip Tile
    base from:
    
    https://gerrit.paranoidandroid.co/457
    
    with moarr changes to OmniRom
    
    toggle Network Flip Tile
    
    for device support 4G (GSM)
    toggle will change into 2G only => 3G only => 2G/3G auto => 2G/3G pref => 4G lte => back to 2G only
    
    for device support 4G (CDMA)
    toggle will change into cdma/evdo auto => cdma only => evdo only => 4G lte => back to cdma/evdo auto
    
    for device unsupport 4G (GSM)
    toggle will change into 2G only => 3G only => 2G/3G auto => 2G/3G pref => back to 2G only
    
    for device unsupport 4G (CDMA)
    toogle will change into cdma/evdo auto => cdma only => evdo only => back to cdma/evdo auto
    
    4G toggle requires that at least one (1) of the following configs
    must be present in the device's build.prop in order to use it:
    telephony.lteOnCdmaDevice=1
    telephony.lteOnGsmDevice=1
    
    Change-Id: I50950399537b19954a4f4fb19d038a07c8759c36

--- src/java/com/android/internal/telephony/BaseCommands.java
+++ src/java/com/android/internal/telephony/BaseCommands.java
@@ -729,6 +729,14 @@ public abstract class BaseCommands implements CommandsInterface {
     }
 
     /**
+     * @hide
+     */
+    @Override
+    public int getLteOnGsmMode() {
+        return TelephonyManager.getLteOnGsmModeStatic();
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override
