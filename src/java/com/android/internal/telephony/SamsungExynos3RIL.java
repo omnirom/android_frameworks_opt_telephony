37826f7d158b0f30bb2b5bf34c066c1afa0c0fea
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-01-16 18:19:50 -0200

    telephony: delay SIM status message until after radio is on
    Change-Id: Idb66ab941f4b12ae13d4dac1fffb1e877d24bb8d
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- src/java/com/android/internal/telephony/SamsungExynos3RIL.java
+++ src/java/com/android/internal/telephony/SamsungExynos3RIL.java
@@ -62,6 +62,7 @@ import android.telephony.Rlog;
 
 public class SamsungExynos3RIL extends RIL implements CommandsInterface {
 
+    private Message mPendingGetSimStatus;
     private boolean mSignalbarCount = SystemProperties.getInt("ro.telephony.sends_barcount", 0) == 1 ? true : false;
     private boolean mIsSamsungCdma = SystemProperties.getBoolean("ro.ril.samsung_cdma", false);
     private Object mCatProCmdBuffer;
@@ -807,6 +808,31 @@ public class SamsungExynos3RIL extends RIL implements CommandsInterface {
                     mDesiredNetworkType = -1;
                 }
             }
+        }
+    }
+
+    // Hack for Lollipop
+    // The system now queries for SIM status before radio on, resulting
+    // in getting an APPSTATE_DETECTED state. The RIL does not send an
+    // RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED message after the SIM is
+    // initialized, so delay the message until the radio is on.
+    @Override
+    public void
+    getIccCardStatus(Message result) {
+        if (mState != RadioState.RADIO_ON) {
+            mPendingGetSimStatus = result;
+        } else {
+            super.getIccCardStatus(result);
+        }
+    }
+
+    @Override
+    protected void switchToRadioState(RadioState newState) {
+        super.switchToRadioState(newState);
+
+        if (newState == RadioState.RADIO_ON && mPendingGetSimStatus != null) {
+            super.getIccCardStatus(mPendingGetSimStatus);
+            mPendingGetSimStatus = null;
         }
     }
 }
