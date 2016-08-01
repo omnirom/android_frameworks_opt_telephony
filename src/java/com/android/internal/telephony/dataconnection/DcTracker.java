68c0b5114b6b02be0aef9c3345f0fe61b5f7b4ba
Yashdev Singh <yashdevs@codeaurora.org>
2016-08-01 16:47:06 -0700

    Telephony: Send INITIAL_ATTACH only when it is applicable.
    Issue is that SET_INITIAL_ATTACH is being sent whenever subId changes.
    
    During bootup subId does change before records gets loaded. This results
    in SET_INITIAL_ATTACH with incorrect APN setting/profile.
    
    Solution is to send SET_INITIAL_ATTACH only when cdmaSubscriptionSource
    is RUIM based and records have been loaded.
    
    Change-Id: I68280db4831935326c732943e5b763d3b3a4c3fe
    
    Fix to send initial attach apn
    
    If there is no IA/default apn then do not perform initial
    attach with any other available apns.
    
    Change-Id: Ie3241747eb44df9070f2cb4874f13878f8ca3d9b
    CRs-Fixed: 1020735

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -545,7 +545,7 @@ public class DcTracker extends Handler {
     // member variables
     protected final Phone mPhone;
     private final UiccController mUiccController;
-    private final AtomicReference<IccRecords> mIccRecords = new AtomicReference<IccRecords>();
+    protected final AtomicReference<IccRecords> mIccRecords = new AtomicReference<IccRecords>();
     private DctConstants.Activity mActivity = DctConstants.Activity.NONE;
     protected DctConstants.State mState = DctConstants.State.IDLE;
     private final Handler mDataConnectionTracker;
@@ -2184,6 +2184,11 @@ public class DcTracker extends Handler {
             }
         }
 
+        if ((iaApnSetting == null) && (defaultApnSetting == null) &&
+                !allowInitialAttachForOperator()) {
+            log("Abort Initial attach");
+            return;
+        }
         // The priority of apn candidates from highest to lowest is:
         //   1) APN_TYPE_IA (Initial Attach)
         //   2) mPreferredApn, i.e. the current preferred apn
@@ -2214,6 +2219,10 @@ public class DcTracker extends Handler {
                     initialAttachApnSetting.protocol, initialAttachApnSetting.authType,
                     initialAttachApnSetting.user, initialAttachApnSetting.password, null);
         }
+    }
+
+    protected boolean allowInitialAttachForOperator() {
+        return true;
     }
 
     /**
