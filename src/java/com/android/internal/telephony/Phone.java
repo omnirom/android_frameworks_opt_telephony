ce7cdf22d69a16984d58ae0d1888bdec0e2864ee
Ravindra <c_rthat@codeaurora.org>
2017-05-08 14:58:42 +0530

    Initiate creation of apn list after loading all the records for CDMA.
    For CDMA if a SIM has CSIM/RUIM and USIM app then create the apnlist after
    SIM and RUIM records are loaded.
    
    Change-Id: Ibb90188dfbb8270844703a7a271e260bae171755
    CRs-Fixed: 2030476

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -70,6 +70,7 @@ import com.android.internal.telephony.uicc.IccCardApplicationStatus.AppType;
 import com.android.internal.telephony.uicc.IccFileHandler;
 import com.android.internal.telephony.uicc.IccRecords;
 import com.android.internal.telephony.uicc.IsimRecords;
+import com.android.internal.telephony.uicc.SIMRecords;
 import com.android.internal.telephony.uicc.UiccCard;
 import com.android.internal.telephony.uicc.UiccCardApplication;
 import com.android.internal.telephony.uicc.UiccController;
@@ -3657,6 +3658,10 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
     public void setCallForwardingOption(int commandInterfaceCFReason,
             int commandInterfaceCFAction, String dialingNumber,
             int commandInterfaceServiceClass, int timerSeconds, Message onComplete) {
+    }
+
+    public SIMRecords getSIMRecords() {
+        return null;
     }
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
