528d89a456228731aa6e55bdbc47c80708859fdb
qiongz <qiongz@codeaurora.org>
2018-05-30 18:11:18 +0800

    Fix the deregistration which happens soon after registration.
    For the origin code logic, GsmCdmaPhone used PHONE_KEY to get phone id
    from the intent after carrier config loaded. Only when phone id from
    intent was equal to the mPhoneId of phone object, phone will start to
    update ims related feature. But now GsmCdmaPhone failed to get PHONE_KEY
    from intent since AOB change.
    
    To fix it, used CarrierConfigManager.EXTRA_SLOT_INDEX to instead of
    PHONE_KEY get correct phone id per new AOB change.
    
    Change-Id: I3913ce38ce237f6090594cd31de3ddc298825d13
    CRs-Fixed: 2254285

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -240,7 +240,9 @@ public class GsmCdmaPhone extends Phone {
         public void onReceive(Context context, Intent intent) {
             Rlog.d(LOG_TAG, "mBroadcastReceiver: action " + intent.getAction());
             if (intent.getAction().equals(CarrierConfigManager.ACTION_CARRIER_CONFIG_CHANGED) &&
-                    intent.getExtras().getInt(PhoneConstants.PHONE_KEY) == mPhoneId) {
+                    intent.getExtras() != null &&
+                    intent.getExtras().getInt(CarrierConfigManager.EXTRA_SLOT_INDEX,
+                    SubscriptionManager.INVALID_SIM_SLOT_INDEX) == mPhoneId) {
                 sendMessage(obtainMessage(EVENT_CARRIER_CONFIG_CHANGED));
             }
         }
